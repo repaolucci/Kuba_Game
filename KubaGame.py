1f42251e06be8ba46e755d1a6b42689ed696de32
Rebecca Paolucci <repaolucci@gmail.com>
2021-06-07 22:47:20 -0500

    changed player_1_captured and player_2_captured to 0

--- KubaGame.py
+++ KubaGame.py
@@ -19,8 +19,8 @@ class KubaGame:
         self._player_2_color = player_2_tuple[1]
         self._current_turn = None
         self._winner = None
-        self._player_1_captured = None
-        self._player_2_captured = None
+        self._player_1_captured = 0
+        self._player_2_captured = 0
         self._w_count = 0
         self._b_count = 0
         self._r_count = 0
